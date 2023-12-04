be4eee9f4cc80362bb4d99ecba073bc7fa8bb030
KANG\b2000 <hanqjun2660@gmail.com>
2023-12-04 19:00:28 +0900

    주석 추가

--- src/main/java/com/blog/velogclone/config/WebMvcConfig.java
+++ src/main/java/com/blog/velogclone/config/WebMvcConfig.java
@@ -13,9 +13,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/", "/dashboard");
     }
 
+    /**
+     * 외부 리소스 요청시 핸들러
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/profile/upload/**")
-                .addResourceLocations("file:///C:/profile/upload/");
+        registry.addResourceHandler("/profile/upload/**")       // 이런 형태의 요청이 들어오면
+                .addResourceLocations("file:///C:/profile/upload/");         // 실제 로컬에 저장된 외부 경로를 알려준다.
     }
 }
