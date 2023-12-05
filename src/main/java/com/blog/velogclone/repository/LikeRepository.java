f3b1d7e7614ad25c4c45656377f2077174a5ec42
KANG\b2000 <hanqjun2660@gmail.com>
2023-12-05 16:15:58 +0900

    읽기목록 조회 쿼리 수정

--- src/main/java/com/blog/velogclone/repository/LikeRepository.java
+++ src/main/java/com/blog/velogclone/repository/LikeRepository.java
@@ -13,6 +13,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
     void deleteByReadNo(Long readNo);
 
-    @Query("select m from Like m join fetch m.user join fetch m.post where m.post.postStatus = 'N' ORDER BY m.createDate DESC")
+    @Query("select m from Like m join fetch m.user join fetch m.post where m.post.postStatus = 'N' and m.user.userNo = :userNo ORDER BY m.createDate DESC")
     List<Like> findByUserNo(Long userNo);
 }
